dbbceae0992ba33557999cffcf1f1c80a3aba92a
Jason Ye <jasonye0104@gmail.com>
2023-04-01 19:13:58 -0400

    bug fix for live launch

--- web/src/pages/events.js
+++ web/src/pages/events.js
@@ -9,7 +9,7 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { display } from '@mui/system';
+// import { display } from '@mui/system';
 
 function createCard(imageSource, title, description, location, time) {
   return (
