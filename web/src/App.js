aa1b0a1d3126ebb29d2ddce1ab93e04231c54d90
Jon Klein <jonklein2021@gmail.com>
2023-04-01 13:22:56 -0400

    smol firebase organization quirk

--- web/src/App.js
+++ web/src/App.js
@@ -5,10 +5,12 @@ import NavBar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Events from './pages/events';
-
-
+// Firebase
+import { db } from './utils/firebase';
+import { onValue, ref } from "firebase/database";
 
 function App() {
+
   return (
     <Router>
       <NavBar/>
--- web/src/utils/firebase.js
+++ web/src/utils/firebase.js
@@ -4,11 +4,9 @@ import { getDatabase } from "firebase/database";
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    databaseURL: 'https://hack-for-change-b2184-default-rtdb.firebaseio.com/', // can't access this through process.env for some reason?
     messagingSenderId: process.env.FIREBASE_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID 
+    appId: process.env.FIREBASE_APP_ID
 }
 
 const app = initializeApp(firebaseConfig);
