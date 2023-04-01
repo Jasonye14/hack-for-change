84331c08ecfba470e3d60f9b761477bdb28dcbf7
Alec <ayj.max@gmail.com>
2023-04-01 14:55:46 -0400

    Added broken carousel

--- web/src/pages/home.js
+++ web/src/pages/home.js
@@ -1,19 +1,53 @@
 import React from 'react';
+import Carousel from 'react-material-ui-carousel';
+import { Paper, Button } from '@mui/material'
 
 // CSS
 import './home.css';
 
 // Assets
 import sea_turtle from '../images/sea_turtle.jpg';
+import turtle2 from '../images/turtle2.jpg';
 
 //Material UI
 import Container from '@mui/material/Container';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+
 
 const Home = () => {
   return (
     <div>
-      <img className={"homeImage"} src={sea_turtle} alt=""></img>
-      
+      <Carousel
+        activeIndicatorIconButtonProps={{
+          style: {
+              backgroundColor: 'blue' // 2
+          }
+        }}
+        navButtonsAlwaysVisible
+      >
+        {/* <Card>
+          <CardMedia
+            sx={{ height: 200 }}
+            img={sea_turtle}
+            title="sea_turtle"
+          >
+          </CardMedia>
+
+        </Card>
+        <Card>
+          <CardMedia
+            sx={{ height: 200 }}
+            img={sea_turtle}
+            title="sea_turtle"
+          />
+        </Card> */}
+        
+
+        {/* <img className={"homeImage"} src={sea_turtle} alt=""></img>
+        <img className={"homeImage"} src={turtle2} alt=""></img> */}
+      </Carousel>
+
     </div>
   );
 };
