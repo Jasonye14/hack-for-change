b5162516384f79acef5483e98ba99b7d3c6b724d
Alec <ayj.max@gmail.com>
2023-04-04 21:29:48 -0400

    added basic protected route functionality

--- web/src/App.js
+++ web/src/App.js
@@ -8,25 +8,26 @@ import Home from './pages/home/Home';
 import Events from './pages/events/Events';
 import UserLogin from './pages/login/UserLogin';
 import UserSignUp from './pages/signup/UserSignup';
-import NotFound from './pages/ not-found/NotFound';
+import NotFound from './pages/not-found/NotFound';
+
+// Special Components
+import ProtectedRoute from './components/ProtectedRoute';
 
 // Database
-import db from './../utils/firebase';
-import { onValue, ref } from "firebase/database";
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 function App(props) {
+  const [uID, setUID] = useState("");
+
   useEffect(() => {
     document.cookie = 'loggedin=false'; // store auth state as cookie
   })
-
-  const [uid, setUid] = useState("");
 
   useEffect(() => {
     const auth = getAuth();
     onAuthStateChanged(auth, user => {
       if (user) { // if user is signed in
-        setUid(user.uid);
+        setUID(user.uid);
       } else {
         window.location.href = "/NotFound";
       }
@@ -40,7 +41,13 @@ function App(props) {
         <Route path='/' exact element={<Home />} />
         <Route path='/login' element={<UserLogin />} />
         <Route path='/signup' element={<UserSignUp />} />
-        <Route path="/users/:username" element={<Events />} />
+        <Route path="/users/:username"
+          element={
+            <ProtectedRoute uID={uID}>
+              <Events/>
+            </ProtectedRoute>
+          }
+        />
         <Route path="/NotFound" element={<NotFound />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
--- web/src/components/ProtectedRoute/index.js
+++ web/src/components/ProtectedRoute/index.js
@@ -3,9 +3,14 @@ import { Navigate } from 'react-router-dom';
 
 // this.props.match.params.id
 
-const ProtectedRoute = ({ user, children }) => {
-    if (!user) {
+const ProtectedRoute = ({ uID, children }) => {
+    if (uID === "") {
         return <Navigate to='/NotFound' replace></Navigate>;
+    }
+    else {
+        return (
+           {children}
+        );
     }
 };
 
