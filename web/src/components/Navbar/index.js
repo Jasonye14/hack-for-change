8489aa528ee7b09044265406d036a978dd447686
Jason Ye <jasonye0104@gmail.com>
2023-09-03 21:40:22 -0400

    added more options to the main menu bar

--- web/src/components/Navbar/index.js
+++ web/src/components/Navbar/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { 
     Nav,
     NavLink,
@@ -6,24 +6,42 @@ import {
     NavMenu,
 } from './NavbarElement';
 
-
 const Navbar = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
 
   useEffect(() => {
-
-  });
-
+    // You can add any side effects or cleanup logic here if required.
+  }, []);
 
   return (
-    <>
+    <nav aria-label="Main Navigation">
       <Nav>
-        <Bars/>
-        <NavMenu>
-          <NavLink to='/'>Home</NavLink>
-          <NavLink to='/login' style={{marginLeft: "auto", marginRight: "3rem",}}>Login</NavLink>
+        <Bars onClick={toggleMobileMenu} aria-label="Toggle mobile menu"/>
+        <NavMenu isOpen={isMobileMenuOpen}>
+          <NavLink to='/' aria-label="Home">Home</NavLink>
+          <NavLink to='/events' aria-label="Events">Events</NavLink>
+          <NavLink to='/community' aria-label="Community">Community</NavLink>
+          <NavLink to='/donations' aria-label="Donations">Donate</NavLink>
+          <NavLink to='/volunteer' aria-label="Volunteer">Volunteer</NavLink>
+          <NavLink to='/resources' aria-label="Resources">Resources</NavLink>
+          <NavLink to='/faq' aria-label="FAQ">FAQ</NavLink>
+          <NavLink 
+            to='/login' 
+            style={{marginLeft: "auto", marginRight: "3rem"}}
+            aria-label="Login"
+            onMouseEnter={(e) => e.target.style.opacity = 0.7}
+            onMouseLeave={(e) => e.target.style.opacity = 1}
+          >
+            Login
+          </NavLink>
         </NavMenu>
       </Nav>
-    </>
+    </nav>
   );
 };
+
 export default Navbar;
