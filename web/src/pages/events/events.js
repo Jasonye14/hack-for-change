5faa1a5400cc47d991ece1f772eaeecc0b6b93b5
Alec Jang <ayj.max@gmail.com>
2023-09-26 13:37:31 -0400

    Basic Comments component

--- web/src/pages/events/events.js
+++ web/src/pages/events/events.js
@@ -10,39 +10,39 @@ const eventCategories = [
 ];
 
 function EventCard({ title, description }) {
-    return (
-        <Card style={{ maxWidth: 345 }}>
-            <CardContent>
-                <Typography variant="h5" component="div">
-                    {title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    {description}
-                </Typography>
-            </CardContent>
-            <CardActions>
-                {/* <Button size="small" color="primary" startIcon={<EcoIcon />}>
-                    Join Now
-                </Button> */}
-            </CardActions>
-        </Card>
-    );
+  return (
+    <Card style={{ maxWidth: 345 }}>
+      <CardContent>
+        <Typography variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        {/* <Button size="small" color="primary" startIcon={<EcoIcon />}>
+            Join Now
+        </Button> */}
+      </CardActions>
+    </Card>
+  );
 }
 
 function Events() {
-    return (
-        <div className="events-container">
-            <h1>Events Page</h1>
-            <p>Welcome to the events page. Here you can view and manage your events.</p>
-            <Grid container spacing={4}>
-                {eventCategories.map((event, index) => (
-                    <Grid item xs={12} sm={6} md={4} key={index}>
-                        <EventCard title={event.title} description={event.description} />
-                    </Grid>
-                ))}
-            </Grid>
-        </div>
-    );
+  return (
+    <div className="events-container">
+      <h1>Events Page</h1>
+      <p>Welcome to the events page. Here you can view and manage your events.</p>
+      <Grid container spacing={4}>
+        {eventCategories.map((event, index) => (
+          <Grid item xs={12} sm={6} md={4} key={index}>
+            <EventCard title={event.title} description={event.description} />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
 }
 
 export default Events;
