7f2f9ff3d61ee85cd3679d2c9ccd461c5b324d6c
Jason Ye <jasonye0104@gmail.com>
2023-09-29 15:47:56 -0400

    added filtering comnditions and styles

--- web/src/components/EventsBar/EventsBar.js
+++ web/src/components/EventsBar/EventsBar.js
@@ -1,7 +1,22 @@
 import React, { useState } from 'react';
 import './EventsBar.css';
+
 function EventBar() {
   const [searchTerm, setSearchTerm] = useState('');
+  const [selectedTags, setSelectedTags] = useState([]);
+
+  const handleDropdownChange = (e) => {
+    if (e.target.value && !selectedTags.includes(e.target.value)) {
+      setSelectedTags(prevTags => [...prevTags, e.target.value]);
+    }
+
+    // Reset the dropdown value
+    // e.target.value = '';
+  }
+
+  const removeTag = (tagToRemove) => {
+    setSelectedTags(prevTags => prevTags.filter(tag => tag !== tagToRemove));
+  }
 
   return (
     <div className="event-bar">
@@ -16,26 +31,37 @@ function EventBar() {
         />
       </div>
 
+      {/* Tags Display */}
+      <div className="tags-container">
+        {selectedTags.map(tag => (
+          <div className="tag" key={tag}>
+            {tag}
+            <span className="remove-tag" onClick={() => removeTag(tag)}>x</span>
+          </div>
+        ))}
+      </div>
+
       {/* Dropdown */}
-      <select className="dropdown">
-        <option value="option1">Option 1</option>
-        <option value="option2">Option 2</option>
-        <option value="option3">Option 3</option>
-        {/* Add more options as needed */}
+      {/* Note: Repeated dropdowns for demonstration; consider abstracting to a reusable component */}
+      <select className="dropdown" onChange={handleDropdownChange}>
+        <option value="">Locations</option>
+        <option value="Bethlehem">Bethlehem</option>
+        <option value="New York">New York</option>
+        <option value="San Francisco">San Francisco</option>
       </select>
 
-      <select className="dropdown">
-        <option value="option1">Option 1</option>
-        <option value="option2">Option 2</option>
-        <option value="option3">Option 3</option>
-        {/* Add more options as needed */}
+      <select className="dropdown" onChange={handleDropdownChange}>
+        <option value="">Select an option...</option>
+        <option value="Option 4">Option 4</option>
+        <option value="Option 5">Option 5</option>
+        <option value="Option 6">Option 6</option>
       </select>
 
-      <select className="dropdown">
-        <option value="option1">Option 1</option>
-        <option value="option2">Option 2</option>
-        <option value="option3">Option 3</option>
-        {/* Add more options as needed */}
+      <select className="dropdown" onChange={handleDropdownChange}>
+        <option value="">Select an option...</option>
+        <option value="Option 1">Option 1</option>
+        <option value="Option 2">Option 2</option>
+        <option value="Option 3">Option 3</option>
       </select>
     </div>
   );
