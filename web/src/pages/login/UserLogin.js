2ecd53d08cfa0e7fe38d2daedc1920909e6ba388
Jon Klein <jonklein2021@gmail.com>
2023-10-20 23:33:19 -0400

    minor things before we move to branches

--- web/src/pages/login/UserLogin.js
+++ web/src/pages/login/UserLogin.js
@@ -12,7 +12,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Alert, AlertTitle} from '@mui/material';
+import { Alert, AlertTitle } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 // Components
