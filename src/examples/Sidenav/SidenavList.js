c9bdf317e103bd059f3561d30633d7aa351a8ee7
Jason Ye <jasonye0104@gmail.com>
2023-12-04 16:44:22 -0500

    added examples

---
+++ src/examples/Sidenav/SidenavList.js
@@ -0,0 +1,25 @@
+// prop-types is a library for typechecking of props.
+import PropTypes from "prop-types";
+
+// @mui material components
+import List from "@mui/material/List";
+
+function SidenavList({ children }) {
+  return (
+    <List
+      sx={{
+        px: 2,
+        my: 0.3,
+      }}
+    >
+      {children}
+    </List>
+  );
+}
+
+// Typechecking props for the SidenavItem
+SidenavList.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default SidenavList;
